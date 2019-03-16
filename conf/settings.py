10920abd9a2b40f38f6ee6e63a63c1576ac8e9c1
Christina <Christinepeninsula@hotmail.com>
2019-03-16 18:59:08 -0400

    deploy

--- conf/settings.py
+++ conf/settings.py
@@ -104,12 +104,12 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 #    }
 # else:
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 
 
 # Password validation
