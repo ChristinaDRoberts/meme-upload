2a031dfeccac03f945a4a15ca108deece536b1e9
Christina <Christinepeninsula@hotmail.com>
2019-03-31 10:54:17 -0400

    heroku

--- conf/settings.py
+++ conf/settings.py
@@ -160,6 +160,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'frontend/static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'frontend/static/media')
 
 MEDIA_URL = '/media/'
