07c2c8f7aae5ee501a9384022520b14e5de700ea
Christina <Christinepeninsula@hotmail.com>
2019-03-31 11:42:03 -0400

    no input

--- conf/settings.py
+++ conf/settings.py
@@ -117,8 +117,13 @@ else:
 
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'meme',
+            'USER': 'meme',
+            'PASSWORD': '',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
         }
     }
 
