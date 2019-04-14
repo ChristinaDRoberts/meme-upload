ea9bbacfbb4b281ee4eb7c86577498baf5dfd854
Christina <Christinepeninsula@hotmail.com>
2019-04-14 15:02:52 -0400

    deploy

--- conf/settings.py
+++ conf/settings.py
@@ -32,7 +32,7 @@ STATICFILES_FINDERS = [
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/static/build/static'),
 )
 
 
